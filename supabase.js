da479dfbe368424bdc7baa0d262b8dc25247d09e
firecamp-official <firecamp.community@gmail.com>
2025-10-28 14:29:16 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
-  "https://YOUR_PROJECT_URL.supabase.co",
-  "YOUR_ANON_KEY"
+  "https://erhfzeejhbxccfmkozxv.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVyaGZ6ZWVqaGJ4Y2NmbWtvenh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2NTQxNTksImV4cCI6MjA3NzIzMDE1OX0.cwJ64wqkaK8ZqJMavYYUABMVzujS3gMibJf-MeBD6pA"
 );
